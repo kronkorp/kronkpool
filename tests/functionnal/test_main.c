4ec0b8809eb936baa6e15810b7798442d9996523
GauthierMalfilatre <gauthier.malfilatre@epitech.eu>
2026-08-29 14:56:45 +0200

    [ADD] Add some functionnals tests

--- tests/functionnal/test_main.c
+++ tests/functionnal/test_main.c
@@ -1,19 +1,33 @@
 #include "kronkpool/kronkpool.h"
+#include <pthread.h>
+#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 static void *__print(void *a)
 {
     for (int i = 0; i < (long int)a; ++i) {
-        printf("%ld\n", (long int)a);
+        printf("hello from %zu\n", pthread_self());
     }
     return NULL;
 }
 
+__attribute__((constructor))
+void init_rand(void)
+{
+    srand(time(NULL));
+}
+
 int main(void)
 {
+    srand(time(NULL));
     kpThreadPool *p = kpThreadPool_create(10);
 
-    kpThreadPool_pushTask(p, &__print, (void *)100);
+    for (size_t i = 0; i < 1000; ++i) {
+        // kpThreadPool_pushTask(p, &__print, (void *)10000);
+        __print((void *)(10000));
+    }
     kpThreadPool_destroy(p);
     return 0;
 }
